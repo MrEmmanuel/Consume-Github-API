79225b787116a0c25d2abc7c7b9a7229049e3c87
MrEmmanuel <sejesoo@gmail.com>
2022-05-25 01:26:43 +0200

    added all initial java files

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Application {
+
+    public static void main(String[] args) {
+        List<PullRequests> pullRequests = new ConsumeGithubAPI().getPullRequests(
+                "Umuzi-org",
+                "ACN-syllabus",
+                "2022-05-01",
+                "2022-05-30"
+        );
+        System.out.println(pullRequests);
+    }
+}
---
+++ src/main/java/ConsumeGithubAPI.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class ConsumeGithubAPI {
+    public List<PullRequests> getPullRequests(String s, String s1, String s2, String s3) {
+
+    }
+}
