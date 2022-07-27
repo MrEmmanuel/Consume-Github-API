e59537178f848c340da27a304f271acc51ef4ce7
Oreneile Sejeso <sejesoo@gmail.com>
2022-07-27 05:20:02 +0200

    Update PullRequests.java

--- src/main/java/PullRequests.java
+++ src/main/java/PullRequests.java
@@ -3,19 +3,13 @@ public class PullRequests {
     String user;
     String title;
     String state;
-    String dateCreated;
+    String created_at;
 
     public PullRequests(String id, String user, String title, String state, String dateCreated){
         this.id = id;
         this.user = user;
         this.title = title;
         this.state = state;
-        this.dateCreated = dateCreated;
+        this.created_at = dateCreated;
     }
-    public String getId(){return this.id;}
-    public String getUser(){return this.user;}
-    public String getTitle(){return this.title;}
-    public String getState(){return this.state;}
-    public String getDateCreated(){return this.dateCreated;}
-
 }
