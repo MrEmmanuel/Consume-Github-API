97bb606095f35f43fc735f0507db9b63053591b1
Oreneile Sejeso <sejesoo@gmail.com>
2022-07-27 05:20:59 +0200

    Made requested changes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -7,9 +9,20 @@ public class Application {
         List<PullRequests> pullRequests = new ConsumeGithubAPI().getPullRequests(
                 "Umuzi-org",
                 "ACN-syllabus",
-                "2022-05-01",
-                "2022-05-30"
+                "2022-03-01",
+                "2022-03-10"
         );
-        System.out.println(pullRequests);
+        System.out.println("[");
+        for(int i=0; i<pullRequests.size(); i++) {
+            System.out.print("{");
+            System.out.print("id: " + pullRequests.get(i).id + ",");
+            System.out.print("user: " + pullRequests.get(i).user + ",");
+            System.out.print("title: " + pullRequests.get(i).title + ",");
+            System.out.print("state: " + pullRequests.get(i).state + ",");
+            System.out.print("created_at: " + pullRequests.get(i).created_at);
+            System.out.print("}");
+            System.out.println();
+        }
+        System.out.print("]");
     }
 }
